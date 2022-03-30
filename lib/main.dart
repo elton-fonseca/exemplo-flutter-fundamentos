80c8c5b868210b90564fe9a42bc4beded24ec14e
Elton Fonseca <elton869@gmail.com>
2022-03-30 16:25:52 -0300

    Criando o Olá mundo no Flutter

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Sorteador());
+}
+
+class Sorteador extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Olá Mundo'),
+    );
+  }
+}
