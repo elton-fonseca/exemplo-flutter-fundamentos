4e4e3d98cd2cb49ee61df3e007f278279995d7c4
Elton Fonseca <elton869@gmail.com>
2022-03-30 16:38:06 -0300

    Criando nosso componente Statefull

--- lib/main.dart
+++ lib/main.dart
@@ -8,20 +8,32 @@ class Sorteador extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Sorteador'),
-        ),
-        body: Column(
-          children: [
-            Text('Número sorteado'),
-            Text('Aperte o botão para sortear'),
-            RaisedButton(
-              onPressed: () => {print('Clicou no botão')},
-              child: Text('Sortear'),
-            ),
-          ],
-        ),
+      home: PaginaInicial(),
+    );
+  }
+}
+
+class PaginaInicial extends StatefulWidget {
+  @override
+  _PaginaInicialState createState() => _PaginaInicialState();
+}
+
+class _PaginaInicialState extends State<PaginaInicial> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sorteador'),
+      ),
+      body: Column(
+        children: [
+          Text('Número sorteado'),
+          Text('Aperte o botão para sortear'),
+          RaisedButton(
+            onPressed: () => {print('Clicou no botão')},
+            child: Text('Sortear'),
+          ),
+        ],
       ),
     );
   }
